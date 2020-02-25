645f77ce6f8c450f5e03fb8b691c3c04a54502a5
Ivan <imartinez@proveedores.com>
2020-02-25 15:31:22 +0100

    Twig update

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,11 +5,28 @@
     namespace App\Controller;
 
 
+    use App\Entity\Post;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
 
-    final class PostController
+    final class PostController extends AbstractController
     {
         public function home() {
-            return new Response('Hello tech yogi friends!!');
+            return $this->render('posts/home.html.twig');
+        }
+
+        public function showPost($post, EntityManagerInterface $em) {
+            $repository = $em->getRepository(Post::class);
+            /** @var Post $postObject */
+            $postObject = $repository->findBy(['post_name' =>$post]);
+            if(!$postObject) {
+                throw $this->createNotFoundException('No found!');
+            }
+            //var_dump($postObject);die;
+            return $this->render('posts/post.html.twig', [
+                'post'=>$postObject
+            ]);
         }
     }
